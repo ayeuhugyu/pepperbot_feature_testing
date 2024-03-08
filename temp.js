7c86bcb2c50497dd79a07d0df10be8915a22bcd3
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2024-03-08 18:33:45 +0000

    randomdeepwokenname.js

--- temp.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs"
-const file = fs.readFileSync("names.txt", "utf8")
-const lines = file.split("\n")
-const firstNames = []
-lines.forEach((line) => {
-    firstNames.push(line.toString().trim())
-})
-fs.writeFileSync("temp.txt", JSON.stringify(firstNames))
